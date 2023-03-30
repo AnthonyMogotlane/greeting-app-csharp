f53ebfc5b44afc1790c99ba7f4a37c97a26859ed
Anthony <almogotlane@gmail.com>
2023-03-30 16:49:41 +0200

    Added id and language properties

--- GreetingApp/Model/Table.cs
+++ GreetingApp/Model/Table.cs
@@ -1,7 +1,12 @@
-namespace GreetingApp.Models;
+using System.ComponentModel.DataAnnotations;
 
+namespace GreetingApp.Models;
 public class Table
 {
-    public string Names { get; set; } = string.Empty; 
-    public int Number{ get; set; }
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Names { get; set; } = string.Empty;
+    public int Number { get; set; }
+    public string? Language { get; set; }
 }
